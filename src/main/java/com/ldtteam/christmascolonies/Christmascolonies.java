fff77be60fec29ff390bd884ffa098b17e76b4ba
Raycoms <ray.neiheiser@gmail.com>
2024-11-23 21:12:21 +0100

    1.21

---
+++ src/main/java/com/ldtteam/christmascolonies/Christmascolonies.java
@@ -0,0 +1,12 @@
+package com.ldtteam.christmascolonies;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("christmascolonies")
+public class Christmascolonies
+{
+    public Christmascolonies()
+    {
+
+    }
+}
